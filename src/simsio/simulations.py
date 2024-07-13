rgbmrc/simsio

commit 300a0a1a2d498db3cc502ee8d248bfe491d0f149

🐛 missing substitutions - to ~

src/simsio/simulations.py
@@ -76,7 +76,7 @@ def _valid_uuid(uid=None, raise_invalid=False):
             raise ValueError from e
         else:
             uid = uuid.uuid1()
-    return str(uid).replace("-", "")
+    return str(uid).replace("~", "")
 
 
 UID_DTYPE = np.array(_valid_uuid()).dtype
@@ -445,7 +445,7 @@ class Simulation(Cache):
         # init Cache & link rc I/O
         super().__init__(readonly=readonly)
 
-        self.uid = uid.rsplit("-", 1)[0] if readonly else _valid_uuid(uid)
+        self.uid = uid.rsplit("~", 1)[0] if readonly else _valid_uuid(uid)
         self._save_time = None
         self._cpu_clock = time.process_time()
         self.cfg_path = None

commit 2be0a02116dcf1adaa5735269182f2af2648441f

🚑 revert bug introduced in 300a0a1a2d498db3cc502ee8d248bfe491d0f149

src/simsio/simulations.py
@@ -76,7 +76,7 @@ def _valid_uuid(uid=None, raise_invalid=False):
             raise ValueError from e
         else:
             uid = uuid.uuid1()
-    return str(uid).replace("~", "")
+    return str(uid).replace("-", "")
 
 
 UID_DTYPE = np.array(_valid_uuid()).dtype