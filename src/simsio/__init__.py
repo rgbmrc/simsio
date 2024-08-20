6a062cd07ac3af57c6b6e65f97339f6610b5bf55
Marco Rigobello <marco.rigobello@hotmail.com>
2024-08-20 11:02:29 +0200

    💡 ConfigParser: note from python docs

--- src/simsio/__init__.py
+++ src/simsio/__init__.py
@@ -4,6 +4,8 @@ __version__ = "0.3.0"
 
 RC_FILE = ".simsiorc"
 rc = ConfigParser(interpolation=None)
+# use read_file instead?
+# https://docs.python.org/3/library/configparser.html#configparser.ConfigParser.read
 rc.read(RC_FILE)
 
 from simsio.simulations import *
