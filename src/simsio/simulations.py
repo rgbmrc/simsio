1d67d1941a5b619196e9e4c17f89ac9064d9c5fb
Marco Rigobello <marco.rigobello@hotmail.com>
2024-08-24 15:55:00 +0200

    🦺 get_sim more consistent null simulation behaviour

--- src/simsio/simulations.py
+++ src/simsio/simulations.py
@@ -228,12 +228,12 @@ def get_sim(sim_or_uid, group=None):
     The eventual Simulation initialization uses default arguments
     (except for group, if provided).
     """
-    if isinstance(sim_or_uid, Simulation):
+    if isinstance(sim_or_uid, Simulation) or sim_or_uid is np.ma.masked:
         return sim_or_uid
     if not isinstance(sim_or_uid, str):
         raise TypeError(f"Expected str uid, got {type(sim_or_uid).__name__}")
     if not sim_or_uid:
-        return None
+        return np.ma.masked
     if sim_or_uid not in sim_registry:
         sim_registry[sim_or_uid] = Simulation(sim_or_uid, group)
         logger.debug(f"Cached simulation {sim_or_uid}")
